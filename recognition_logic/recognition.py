b3c6c9de12d057ebdda6b80a3461be6cc356c2da
Valentyn <valentyncherkasov24@gmail.com>
2021-12-14 19:22:43 +0200

    feat: add train function and comparing

--- recognition_logic/recognition.py
+++ recognition_logic/recognition.py
@@ -8,7 +8,11 @@ class Recognizer:
     def picture_face_recognition(self, image_path):
         image = face_recognition.load_image_file(image_path)
         face_locations = face_recognition.face_locations(image)
-        return (image, face_locations)
+        if face_locations:
+            print(f"The face is detected on image '{image_path}'.")
+            return (image, face_locations)
+        else:
+            print("There is no face on this image.")
 
     def mark_faces(self, loaded_image, face_coordinates, destination):
         image = Image.fromarray(loaded_image)
@@ -22,7 +26,18 @@ class Recognizer:
         del draw
         image.save(destination)
 
-    def recognition_runner(self, input_path, output_path):
+    def compare_two_faces(self, loaded_img1, loaded_img2):
+        img1_encoding = face_recognition.face_encodings(loaded_img1)[0]
+        img2_encoding = face_recognition.face_encodings(loaded_img2)[0]
+        result = face_recognition.compare_faces([img1_encoding], img2_encoding)
+
+        if result[0]:
+            print("There is the same person on the images.")
+        else:
+            print("There are different peoples on the images.")
+        return result
+
+    def recognition_run(self, input_path, output_path):
         if not os.path.exists(input_path):
             print("f[ERROR] there is no directory '{input_path}'")
             sys.exit()
@@ -35,8 +50,40 @@ class Recognizer:
                 destination = f"{output_path}/marked_{image_path}"
                 self.mark_faces(image, face_coordinates, destination)
 
+    def train_model_with_images(self, name, input_path):
+        known_encodings = []
+        images = os.listdir(input_path)
+        for image_path in images:
+            image = face_recognition.load_image_file(f"{input_path}/{image_path}")
+            encoding = face_recognition.face_encodings(image)[0]
+            if len(known_encodings) == 0:
+                known_encodings.append(encoding)
+            else:
+                for item in range(0, len(known_encodings)):
+                    result = face_recognition.compare_faces(
+                        [encoding], known_encodings[item]
+                    )
+                    if result[0]:
+                        known_encodings.append(encoding)
+                        print("The same person!")
+                        break
+                    else:
+                        print("Another person!")
+                        break
+
+        data = {"name": name, "encoding": known_encodings}
+        return data
+
     def main(self):
-        self.recognition_runner("input_images", "output_images")
+        # self.recognition_run("input_images", "output_images")
+
+        # self.picture_face_recognition("input_images/laptop_1.jpeg")
+
+        # img1, coordinates1 = self.picture_face_recognition("input_images/laptop_1.jpeg")
+        # img2, coordinates2 = self.picture_face_recognition("input_images/jobs_1.jpg")
+        # self.compare_two_faces(img1, img2)
+
+        # print(self.train_model_with_images("Elon", "input_images"))
 
 
 if __name__ == "__main__":
